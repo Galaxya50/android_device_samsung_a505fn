0968f64883b817eaf4a183ec59328f148dc2b5c9
glikched <florianvictorcristea@gmail.com>
2022-10-04 17:39:19 +0000

    a505fn: Inherit non_ab_device.mk

--- aosp_a505fn.mk
+++ aosp_a505fn.mk
@@ -17,6 +17,7 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 ## Inherit from a505fn device
 $(call inherit-product, device/samsung/a505fn/device.mk)
